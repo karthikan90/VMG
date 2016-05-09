7f69a5b7dc4c9cdd6c08a29ec606ea6b4ac22924
Prashanth <Prashanth@Prashanth-PC>
2016-05-09 23:26:09 +0530

    storing sub category data

--- TestProj/ddl/EnterpriseAPP_SqlServer.sql
+++ TestProj/ddl/EnterpriseAPP_SqlServer.sql
@@ -12,6 +12,10 @@ create table Category(
   category_name varchar(50)
   )
 
+create table SubCategory(
+    sub_category_id int identity primary key,
+                            sub_category_name varchar(50),
+                            fk_category_id int references Category(category_id))
   INSERT INTO [dbo].[Category] ([category_name]) VALUES ('Electronics')
   INSERT INTO [dbo].[Category] ([category_name]) VALUES ('Home Appliances')
   INSERT INTO [dbo].[Category] ([category_name]) VALUES ('Clothing')
